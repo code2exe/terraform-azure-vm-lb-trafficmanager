54131012ce6f1bae48bd62e69a82f290db91f196
code2exe <rxsvwuordbza@dropmail.me>
2021-06-08 07:49:04 +0100

    Integrated Service Principal Authentication for Azure Key Vault Support

--- variables.tf
+++ variables.tf
@@ -10,7 +10,14 @@ variable "tag_name" {
 variable "tenant_id" {
   description = "Azure Tenant ID"
 }
-
+variable "client_id" {
+  type = string
+  description = "Azure Client ID"
+}
+variable "client_secret" {
+  type = string
+  description = "Azure Client Secret"
+}
 variable "location" {
   description = "Azure East location"
 }
@@ -19,13 +26,13 @@ variable "north_location" {
   description = "Azure North location"
 }
 variable "prefix" {
-  description = "Name Convention"
+  description = "Azure Name Convention"
 }
 
 variable "vm_username" {
   description = "Azure VM Username"
 }
 
-variable "vm_password" {
-  description = "Azure VM Password"
+variable "user_object_id" {
+  description = "User's Object ID from Azure Portal"
 }
